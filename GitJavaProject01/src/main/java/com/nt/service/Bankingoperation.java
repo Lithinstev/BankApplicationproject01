d258fcfbe41ca9261f90f68a34c497c9fd1e29e2
ADMIN <ADMIN@DESKTOP-IE5U5U4>
2026-06-13 11:43:28 +0530

    created a  class implemented interface methods

---
+++ GitJavaProject01/src/main/java/com/nt/service/Bankingoperation.java
@@ -0,0 +1,19 @@
+package com.nt.service;
+
+public class Bankingoperation implements FunctionalComp{
+
+	@Override
+	public double withdrawal(double Amount) {
+		// TODO Auto-generated method stub
+		
+		return 0;
+	}
+
+	@Override
+	public double Deposit(double Amount) {
+		// TODO Auto-generated method stub
+		
+		return 0;
+	}
+
+}
