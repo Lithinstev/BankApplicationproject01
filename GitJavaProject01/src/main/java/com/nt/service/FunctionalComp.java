a24930219dc7e9e4049479140179c4a251d3dc4f
Lithin_sai <jallalithins@gmail.com>
2026-06-13 11:38:34 +0530

    modified FunctionalInterface by adding two abstract methods

--- GitJavaProject01/src/main/java/com/nt/service/FunctionalComp.java
+++ GitJavaProject01/src/main/java/com/nt/service/FunctionalComp.java
@@ -1,5 +1,9 @@
 package com.nt.service;
 
-public interface FunctionalComp {
-
+public interface FunctionalComp 
+{
+     public double withdrawal(double Amount) ;
+     public double Deposit(double Amount) ;
+    	 
+     
 }
