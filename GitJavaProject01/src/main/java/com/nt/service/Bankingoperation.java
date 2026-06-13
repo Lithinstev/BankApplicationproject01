039801f36dd62bf8bba299ccb14fb0ea2c0ca26f
ADMIN <ADMIN@DESKTOP-IE5U5U4>
2026-06-13 12:02:03 +0530

    updated withdraw method and written logic inDeposit method

--- GitJavaProject01/src/main/java/com/nt/service/Bankingoperation.java
+++ GitJavaProject01/src/main/java/com/nt/service/Bankingoperation.java
@@ -1,17 +1,17 @@
 package com.nt.service;
 
 public class Bankingoperation implements FunctionalComp{
+	double  current_balance = 2000.00;
 
 	@Override
 	public double withdrawal(double Amount)
 	{
-		double  current_balance = 2000.00;
 		if(Amount <= 0)
 			 throw new IllegalArgumentException("Widhdrawal amount should be in postive");
 		else if(Amount >= current_balance) 
 			throw new IllegalArgumentException("Widhdrawal amount should be less than or equal to current_balance");
 		else 
-		  (current_balance - Amount);			
+		  current_balance -= Amount;			
 	
 		return  current_balance;
 	}
@@ -19,8 +19,9 @@ public class Bankingoperation implements FunctionalComp{
 	@Override
 	public double Deposit(double Amount) 
 	{
-		
-		return 0;
+		current_balance=current_balance+Amount;
+		System.out.println("Deposit Successful");
+		return current_balance;
 	}
 
 }
