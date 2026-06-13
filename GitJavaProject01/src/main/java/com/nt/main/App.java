7e498bef96a9f8b3ab40d6945e47b6c87d6a4608
Lithin_sai <jallalithins@gmail.com>
2026-06-13 11:20:34 +0530

    project is shared

---
+++ GitJavaProject01/src/main/java/com/nt/main/App.java
@@ -0,0 +1,12 @@
+package com.nt.main;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("App.main()");
+        
+    }
+}
