7553a092d47fbffa7e64f75084115421d08446a2
Lithin_sai <jallalithins@gmail.com>
2026-06-13 11:57:22 +0530

    withdrawal Method Logic implemented

--- GitJavaProject01/src/main/java/com/nt/service/Bankingoperation.java
+++ GitJavaProject01/src/main/java/com/nt/service/Bankingoperation.java
@@ -3,15 +3,22 @@ package com.nt.service;
 public class Bankingoperation implements FunctionalComp{
 
 	@Override
-	public double withdrawal(double Amount) {
-		// TODO Auto-generated method stub
-		
-		return 0;
+	public double withdrawal(double Amount)
+	{
+		double  current_balance = 2000.00;
+		if(Amount <= 0)
+			 throw new IllegalArgumentException("Widhdrawal amount should be in postive");
+		else if(Amount >= current_balance) 
+			throw new IllegalArgumentException("Widhdrawal amount should be less than or equal to current_balance");
+		else 
+		  (current_balance - Amount);			
+	
+		return  current_balance;
 	}
 
 	@Override
-	public double Deposit(double Amount) {
-		// TODO Auto-generated method stub
+	public double Deposit(double Amount) 
+	{
 		
 		return 0;
 	}
