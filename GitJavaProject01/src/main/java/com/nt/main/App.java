12b1c514aa7156a25f9f66f0abd36a351e2bc946
Lithin_sai <jallalithins@gmail.com>
2026-06-13 12:04:55 +0530

    accessing b.logic

--- GitJavaProject01/src/main/java/com/nt/main/App.java
+++ GitJavaProject01/src/main/java/com/nt/main/App.java
@@ -1,12 +1,13 @@
 package com.nt.main;
 
+import com.nt.service.Bankingoperation;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("App.main()");
-        
+    public static void main(String[] args)
+     {
+           System.out.println(new Bankingoperation().withdrawal(1000));
     }
 }
